83b74f62b0acd0ad3d4a300ccbf91bad4a5cf079
@Cesarscc <ccoloradoc@uni.pe>
2025-03-05 11:31:05 -0500

    feat: add-puzzle-game

--- src/app/page.js
+++ src/app/page.js
@@ -49,6 +49,7 @@ import DamasGame from '/public/images/DamasGame.png';
 import TestDeveloper from '/public/images/TestDeveloper.png';
 import PruebaReactDeveloper from '/public/images/PruebaReactDeveloper.png';
 import DashboardCompany from '/public/images/DashboardCompany.png';
+import PuzzleGame from '/public/images/PuzzleGame.png';
 import StripeIcon from '@/components/icons/StripeIcon';
 import TypeScript from '@/components/icons/TypeScript';
 import LogoMaterialUi from '@/components/icons/LogoMaterialUi';
@@ -2108,6 +2109,78 @@ export default function Home() {
                       <a
                         target="_blank"
                         href={'https://dashboard-company.netlify.app/'}
+                        className="rounded-xl transition border border-gray-600 bg-gray-800 w-28 h-8 text-center flex justify-center items-center gap-2 hover:border-0 hover:bg-gray-800 hover:border-gray-900 hover:text-white"
+                      >
+                        <p>
+                          <Preview />
+                        </p>
+                        <p className="text-gray-400">Preview</p>
+                      </a>
+                    </footer>
+                  </div>
+                </div>
+              </article>
+              <article className="flex flex-col space-x-0 space-y-8 group md:flex-row md:space-x-8 md:space-y-0">
+                <div className="w-full md:w-1/2">
+                  <div className="relative flex flex-col items-center col-span-6 row-span-5 gap-8 transition duration-500 ease-in-out transform shadow-xl overflow-clip rounded-xl sm:rounded-xl md:group-hover:-translate-y-1 md:group-hover:shadow-2xl lg:border lg:border-gray-800 lg:hover:border-gray-700 lg:hover:bg-gray-800/50">
+                    <Image
+                      alt="Puzzle Game"
+                      className="object-cover object-top w-full h-56 transition duration-500 sm:h-full md:scale-110 md:group-hover:scale-105"
+                      loading="lazy"
+                      width={900}
+                      height={800}
+                      src={PuzzleGame}
+                    />
+                  </div>
+                </div>
+                <div className="w-full md:w-1/2 md:max-w-lg">
+                  <h3 className="text-2xl font-bold text-gray-800 dark:text-gray-100">
+                    Puzzle Game
+                  </h3>
+                  <div className="flex flex-wrap mt-2">
+                    <ul className="grid grid-cols-2 mb-2 gap-x-2">
+                      <li className="flex items-center">
+                        <NextJsIcon />
+                        <strong className="text-white ml-1 text-sm">
+                          Next Js
+                        </strong>
+                      </li>
+                      <li className="flex items-center">
+                        <TypeScript />
+                        <strong className="text-white text-sm">
+                          TypeScript
+                        </strong>
+                      </li>
+                      <li className="flex items-center">
+                        <TailwindIcon />
+                        <strong className="text-white text-sm">
+                          TailwindCss
+                        </strong>
+                      </li>
+                      <li className="flex items-center">
+                        <div>
+                          <Image
+                            alt="Zustan Icon"
+                            loading="lazy"
+                            width={50}
+                            height={50}
+                            src={ZustandIcon}
+                          />
+                        </div>
+                        <strong className="text-white text-sm ml-1">
+                          Zustand
+                        </strong>
+                      </li>
+                    </ul>
+
+                    <div className="mt-2 text-gray-700 dark:text-gray-400">
+                      Juego de completar rompecabezas, con drag and drop o
+                      click.
+                    </div>
+                    <footer className="flex items-end justify-start mt-4 gap-x-4">
+                      <a
+                        target="_blank"
+                        href={'https://puzzle-game-cc.netlify.app/'}
                         className="rounded-xl transition border border-gray-600 bg-gray-800 w-28 h-8 text-center flex justify-center items-center gap-2 hover:border-0 hover:bg-gray-800 hover:border-gray-900 hover:text-white"
                       >
                         <p>
